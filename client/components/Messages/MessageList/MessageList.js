cc594bbb0d81d6ecc606fce8061cadf864e70f74
Adam <adamjking3@gmail.com>
2017-01-27 19:13:30 -0700

    Stylistic update to separate messages by day. Added dateformat to dependencies.

--- client/components/Messages/MessageList/MessageList.js
+++ client/components/Messages/MessageList/MessageList.js
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from 'react';
+import { Divider } from 'semantic-ui-react';
+import dateFormat from 'dateformat';
 import cx from 'classnames';
 
 import { Message } from '../../../containers';
@@ -7,6 +9,7 @@ import styles from './MessageList.css';
 export default class MessageList extends Component {
   render () {
     const { messages, onScroll, onSetRef } = this.props;
+    let lastDate;
     return (
       <div className={styles.messageListContainer}>
         <div
@@ -14,12 +17,21 @@ export default class MessageList extends Component {
           ref={onSetRef}
           onScroll={onScroll}
         >
-          {messages && messages.reverse().map((message) => message && (
-            <Message
-              key={message.id}
-              message={message}
-            />
-          ))}
+          {messages && messages.reverse().map((message) => {
+            const messageDate = dateFormat(message.createdAt, 'mmmm dS');
+            if (lastDate !== messageDate) {
+              lastDate = messageDate;
+              return (
+                <div key={message.id}>
+                  <Divider horizontal>
+                    {messageDate}
+                  </Divider>
+                  <Message message={message} />
+                </div>
+              );
+            }
+            return <Message key={message.id} message={message} />;
+          })}
         </div>
       </div>
     );
